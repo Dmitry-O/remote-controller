48f6a79233766692c021782446fdbf3e44e928a6
Dmitry-O <dimasic9420@gmail.com>
2021-10-25 00:21:56 +0300

    v.1.4.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import './App.css';
 import React from 'react';
-import { FaPowerOff, FaWindowClose, FaExclamationCircle, FaPlus, FaEdit } from 'react-icons/fa';
+import { FaPowerOff, FaWindowClose, FaExclamationCircle, FaPlus, FaEdit, FaCheckCircle } from 'react-icons/fa';
 import {Button, Modal, Input, Label, Form, Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import styles from './shared/styles';
 import baseUrl from './shared/baseUrl';
 import { postController, getController, putController } from './shared/API';
 
+// localStorage.removeItem('names');
+
 let controllers = localStorage.getItem('controllers') ? JSON.parse(localStorage.controllers) : [];
+let names = localStorage.getItem('names') ? JSON.parse(localStorage.names) : [];
 let countRelays = 1;
 
 const RenderControllers = ({aController}) => {
+    const [modalVisible, setModalVisible] = React.useState(false);
     const [dropdownOpen, setDropdownOpen] = React.useState(false);
+    const [isChanged, setIsChanged] = React.useState(false);
+
     const toggle = () => setDropdownOpen(prevState => !prevState);
     let relayArr = [];
-    
 
     for(let i of aController.relays) {
         relayArr.push(i);
     }
 
-    let controller = aController;
+    let controller = aController, relayNames = [], serialName = '';
+
+    if(names.length > 0) {
+        names.some(el => el.serial === controller.serial) ? names.forEach(el => {
+            if(el.serial === controller.serial) {
+                for(let i in el.relays)
+                    relayNames[i] = el.relays[i];    
+                serialName = el.serialName;
+            }
+        })
+        : controller.relays.forEach((el, index) => relayNames[index] = "");
+    }
+
+    const [name, setName] = React.useState({serialName, relays: relayNames, serial: controller.serial});
+
+    const inputs = relayArr.map((el, index) => 
+        <div className="col-5">
+            <div className="row">
+                <Input
+                    key={name.serial}
+                    type="text" maxLength="10"
+                    onChange={e => {
+                        relayNames[index] = e.target.value;
+                        setName({serialName: name.serialName, relays: relayNames, serial: name.serial});
+                        setIsChanged(true);
+                    }}
+                    onBlur={() => {
+                            names.some(el => el.serial === name.serial) ? names.forEach(el => {
+                                if(el.serial === name.serial) {
+                                    for(let i in name.relays)
+                                        el.relays[i] = name.relays[i];                  
+                                }
+                            })
+                            : names.push(name);
+                            localStorage.names = JSON.stringify(names);
+                        }
+                    } 
+                    style={styles.input}
+                    value={name.relays[index] ? name.relays[index] : ""}
+                    placeholder={"Switch " + (1 + index)}
+                />
+                {'(' + (index + 1) + ')'}
+            </div>
+        </div>
+    );
 
     const relays = controller.relays ? controller.relays.map((el, index, arr) =>
         <div className="col-3">
@@ -36,35 +85,90 @@ const RenderControllers = ({aController}) => {
                     })
                 }} key={index}
             >
-                <h5>Switch {index+1} {el ? <FaPowerOff className="text-warning"/> : <FaPowerOff/>} </h5>
+                <h5>
+                    {
+                        relayNames[index] ?
+                            <>
+                                {relayNames[index]} {el ? <FaPowerOff className="text-warning"/> : <FaPowerOff/>}
+                            </>
+                        :
+                            <>
+                                Switch {index+1} {el ? <FaPowerOff className="text-warning"/> : <FaPowerOff/>}
+                            </>
+                    }
+                </h5>
             </DropdownItem>
         </div>
     ) : [];
 
     return (
-        <div className="row mb-1">
-            <div className="col-11">
-                <Dropdown size="lg" className="row" isOpen={dropdownOpen} toggle={toggle} key={controller.serial}>
-                    <DropdownToggle caret color="warning" style={{width: "100%"}}>        
-                        {controller.serial}
-                    </DropdownToggle>
-                    <DropdownMenu right className="text-center row">
-                        <DropdownItem header><h3 className="text-center">Перемикачі</h3></DropdownItem>
-                        <div className="row">{relays}</div>
-                    </DropdownMenu>
-                </Dropdown>
+        <>
+            <div className="row mb-1">
+                <div className="col-10">
+                    <Dropdown size="lg" className="row" isOpen={dropdownOpen} toggle={toggle} key={controller.serial}>
+                        <DropdownToggle caret color="warning" style={{width: "100%"}}>        
+                            {
+                                serialName ? serialName + ' (' + controller.serial + ')'
+                                : controller.serial
+                            }
+                        </DropdownToggle>
+                        <DropdownMenu right className="text-center row">
+                            <DropdownItem header><h3 className="text-center">Перемикачі</h3></DropdownItem>
+                            <div className="row">{relays}</div>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+                <div className="col-2">
+                    <FaEdit onClick={() => setModalVisible(!modalVisible)} style={{cursor: "pointer"}} size="30px" />
+                    <FaWindowClose onClick={() => controllers.forEach((el, index, arr) => {
+                        if (el.serial === controller.serial) {
+                            arr.splice(arr[index], 1);
+                            localStorage.controllers = JSON.stringify(controllers);
+                            window.location.reload();
+                        }
+                    }) } size="30px" className="mt-1" style={{marginLeft: "5px", cursor: "pointer"}}/>
+                </div>
             </div>
-            <div className="col-1">
-                <FaEdit size="30px" />
-                <FaWindowClose onClick={() => controllers.forEach((el, index, arr) => {
-                    if (el.serial === controller.serial) {
-                        arr.splice(arr[index], 1);
-                        localStorage.controllers = JSON.stringify(controllers);
-                        window.location.reload();
-                    }
-                }) } size="30px" className="mt-1" style={{marginLeft: "5px", cursor: "pointer"}}/>
-            </div>
-        </div>
+            <Modal className="" isOpen={modalVisible} toggle={() => setModalVisible(!modalVisible)}>
+                <h4 className="text-center mt-2 text-secondary">Редагування мікроконтролеру</h4>
+                <h4 className="text-center mt-2">Назва</h4>
+                <Input
+                    type="text"
+                    onChange={e => {
+                        setName({serialName: e.target.value, relays: name.relays, serial: name.serial});
+                        setIsChanged(true);
+                    }}
+                    onBlur={() => {
+                        names.some(el => el.serial === name.serial) ? names.forEach(el => {
+                            if(el.serial === name.serial) {
+                                el.serialName = name.serialName;                  
+                            }
+                        })
+                        : names.push(name);
+                        localStorage.names = JSON.stringify(names);
+                    }}
+                    style={styles.input}
+                    placeholder="Назва мікроконтролеру"
+                    value={name.serialName ? name.serialName : ""}
+                />
+                <h4 className="text-center mt-1">Перемикачі</h4>
+                <div className="row" style={{marginLeft: "3em"}}>
+                    {inputs}
+                </div>
+              <Button
+                  color="warning"
+                  className="mb-2 mt-2"
+                  style={{width: "20%", marginLeft: "auto", marginRight: "auto"}}
+              >
+                OK
+              </Button>
+              {
+                  isChanged === true ?
+                    <span className="text-center"><FaCheckCircle color="green"/> Зміни зберігаються автоматично</span>
+                  : null
+              }
+            </Modal>
+        </>
     );
 };
 
